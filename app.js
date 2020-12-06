94286802896560e163007b23d31d515a3b0afb54
Eric <menaericdaniel@gmail.com>
2020-12-06 08:47:31 -0300

    prueba de pago

--- app.js
+++ app.js
@@ -1,22 +1,87 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+var express = require("express");
+var exphbs = require("express-handlebars");
+var port = process.env.PORT || 3000;
 
-var app = express();
- 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+const mercadopago = require("mercadopago");
 
-app.use(express.static('assets'));
- 
-app.use('/assets', express.static(__dirname + '/assets'));
-
-app.get('/', function (req, res) {
-    res.render('home');
+mercadopago.configure({
+  access_token:
+    "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
+  integrator_id: "dev_24c65fb163bf11ea96500242ac130004",
 });
 
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+var app = express();
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use(express.static("assets"));
+
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/detail", function (req, res) {
+  res.render("detail", req.query);
+});
+
+app.post("/buy", function (req, res) {
+  const host = req.protocol + "://" + req.get("host");
+
+  let preference = {
+    payment_methods: {
+      installments: 6,
+      excluded_payment_methods: [
+        {
+          id: "amex",
+        },
+      ],
+      excluded_payment_types: [
+        {
+          id: "atm",
+        },
+      ],
+    },
+    payer: {
+      name: "Lalo",
+      surname: "Landa",
+      email: "test_user_63274575@testuser.com",
+      phone: {
+        area_code: "11",
+        number: 22223333,
+      },
+      address: {
+        street_name: "False",
+        street_number: 123,
+        zip_code: "1111",
+      },
+    },
+    items: [
+      {
+        id: 1234,
+        title: req.body.title,
+        description: "Dispositivo móvil de Tienda e-commerce",
+        picture_url: host + req.body.img,
+        quantity: 1,
+        unit_price: Number(req.body.price),
+      },
+    ],
+    external_reference: "menaericdaniel@gmail.com",
+  };
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      global.id = response.body.id;
+      global.init_point = response.body.init_point;
+      //console.log(response);
+      return res.render("confirm",{
+        init_point : response.body.init_point
+      });
+    })
+    .catch((error) => console.log(error));
 });
 
 app.listen(port);
